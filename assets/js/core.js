07d7980587e531c7f60c1ab6992264f8b5ca2b3f
derpierre65 <git@derpierre65.de>
2020-05-20 23:38:32 +0200

    added dark mode (not implemented for users)

--- assets/js/core.js
+++ assets/js/core.js
@@ -28,7 +28,41 @@
 			}
 		}
 	};
+	var _darkModeLink = null;
 
 	window.Core = {};
+	window.Core.DarkMode = {
+		enable: function () {
+			if (!_darkModeLink) {
+				_darkModeLink = $('<link href="assets/css/bootstrap_slate.min.css" rel="stylesheet">');
+				$('head').append(_darkModeLink);
+			}
+		},
+		disable: function () {
+			if (_darkModeLink) {
+				_darkModeLink.remove();
+				_darkModeLink = null;
+			}
+		},
+		toggle: function () {
+			if (!_darkModeLink) {
+				this.enable();
+			}
+			else {
+				this.disable();
+			}
+
+			if (typeof window.localStorage !== 'undefined') {
+				window.localStorage.setItem('darkMode', _darkModeLink ? '1' : '0');
+			}
+		}
+	};
 	window.Core.AjaxStatus = AjaxStatus;
+
+	if (typeof window.localStorage !== 'undefined') {
+		let darkMode = window.localStorage.getItem('darkMode');
+		if (darkMode === '1') {
+			window.Core.DarkMode.enable();
+		}
+	}
 })();
