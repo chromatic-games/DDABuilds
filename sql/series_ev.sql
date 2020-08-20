de1cd97dea26de8ebd45520ab30779f1440ae16b
derpierre65 <git@derpierre65.de>
2020-08-20 18:10:22 +0200

    * updated series ev-a mana costs - old sql files

--- sql/series_ev.sql
+++ sql/series_ev.sql
@@ -5,8 +5,8 @@ INSERT INTO classes (id, name, isHero)
 	VALUES ('5', 'Series EV-A', '1');
 
 INSERT INTO towers (id, mu, manacost, fk_class, name, unitcost, maxUnitCost)
-	VALUES (21, 0, 0, 5, 'Proton Beam', 2, 5),
-	       (22, 0, 0, 5, 'Blocking Field', 2, 5),
-	       (23, 0, 0, 5, 'Reflect Beam', 1, 3),
-	       (24, 0, 0, 5, 'Shock Beam', 2, 6),
-	       (25, 0, 0, 5, 'Overclock Beam', 4, 6);
+	VALUES (21, 0, 40, 5, 'Proton Beam', 2, 5),
+	       (22, 0, 30, 5, 'Blocking Field', 2, 5),
+	       (23, 0, 20, 5, 'Reflect Beam', 1, 3),
+	       (24, 0, 55, 5, 'Shock Beam', 2, 6),
+	       (25, 0, 70, 5, 'Overclock Beam', 4, 6);
