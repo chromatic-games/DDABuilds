9fe64a4f48923b255f6c9e26a4e01abe9be6893a
derpierre65 <git@derpierre65.de>
2020-10-07 22:41:36 +0200

    * moved vue source files from js/ to src/

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 
 mix
-	.js('resources/js/main.js', 'public/assets/js')
+	.js('resources/src/main.js', 'public/assets/js')
 	.sass('resources/style/style.scss', 'public/assets/css');
 
 if (mix.inProduction()) {
