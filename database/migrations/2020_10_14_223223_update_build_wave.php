b185d709b1fbf022b555700f0daba8a6a161f186
derpierre65 <git@derpierre65.de>
2021-05-13 19:51:24 +0200

    move migration commands to migrations

--- database/migrations/2020_10_14_223223_update_build_wave.php
+++ database/migrations/2020_10_14_223223_update_build_wave.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Build;
+use App\Models\Build\BuildTower;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,6 +13,49 @@ class UpdateBuildWave extends Migration {
 	 * @return void
 	 */
 	public function up() {
+		$builds = Build::query()->with('waves')->get();
+
+		/** @var Build $build */
+		foreach ( $builds as $build ) {
+			// get build waves
+			$waves = $build->waves()->orderBy('waveID')->get()->pluck('waveID');
+
+			// delete old towers
+			BuildTower::query()
+				->where([
+					'fk_build' => $build->getKey(),
+				])
+				->where('fk_buildwave', '>', count($waves))
+				->delete();
+
+			// update waves there already exists with the wave id
+			foreach ( $waves as $key => $waveID ) {
+				BuildTower::query()
+					->where([
+						'fk_build' => $build->getKey(),
+						'fk_buildwave' => $key + 1,
+					])
+					->update([
+						'fk_buildwave' => $waveID,
+					]);
+			}
+
+			// create the wave 0 as real wave
+			$wave = $build->waves()->create([
+				'name' => 'Build',
+			]);
+
+			// update towers with wave 0
+			BuildTower::query()
+				->where([
+					'fk_build' => $build->getKey(),
+					'fk_buildwave' => 0,
+				])
+				->update([
+					'fk_buildwave' => $wave->getKey(),
+				]);
+		}
+
 		Schema::table('build_wave', function (Blueprint $table) {
 			$table->unsignedInteger('fk_build')->after('id')->change();
 		});
--- database/migrations/2020_10_28_215304_update_steam_user.php
+++ database/migrations/2020_10_28_215304_update_steam_user.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Models\Build;
+use App\Models\Issue;
+use App\Models\IssueComment;
+use App\Models\SteamUser;
+use App\Services\Steam;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,6 +21,45 @@ class UpdateSteamUser extends Migration
 	    Schema::table('steam_user', function (Blueprint $table) {
 		    $table->renameColumn('steamID', 'ID');
 	    });
+
+	    $builds = Build::query()->whereDoesntHave('user')->get();
+	    $buildComments = Build\BuildComment::query()->whereDoesntHave('user')->get();
+	    $issues = Issue::query()->whereDoesntHave('user')->get();
+	    $issueComments = IssueComment::query()->whereDoesntHave('user')->get();
+	    $steamIDs = array_unique(array_merge(
+		    ($builds->pluck('steamID')->all()),
+		    ($issues->pluck('steamID')->all()),
+		    ($issueComments->pluck('steamID')->all()),
+		    ($buildComments->pluck('steamID')->all())
+	    ));
+
+	    if ( count($steamIDs) === 0 ) {
+		    return;
+	    }
+
+	    /** @var Steam $steam */
+	    $steam = app(Steam::class);
+
+	    $steamUsers = [];
+	    foreach ( $steamIDs as $steamID ) {
+		    $steamUsers[$steamID] = SteamUser::query()->forceCreate([
+			    'ID' => $steamID,
+			    'name' => 'Unknown User',
+			    'avatarHash' => '',
+		    ]);
+	    }
+
+	    foreach ( $steamIDs as $steamID ) {
+		    $userInfo = $steam->getUserInfo($steamID);
+		    if ( $userInfo === null ) {
+			    continue;
+		    }
+
+		    $steamUsers[$steamID]->update([
+			    'name' => $userInfo['personaname'],
+			    'avatarHash' => $userInfo['personaname'],
+		    ]);
+	    }
     }
 
     /**
