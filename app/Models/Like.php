f68ba23a78949400e5d5c1ffc1890f4060e4af56
derpierre65 <git@derpierre65.de>
2021-05-13 20:22:13 +0200

    add HasSteamUser to like model

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasSteamUser;
+
 /**
  * @property-read string $objectType
  * @property-read int $objectID
@@ -11,6 +13,8 @@ namespace App\Models;
  * @property-read string $notificationID
  */
 class Like extends AbstractModel {
+	use HasSteamUser;
+
 	protected $table = 'like';
 
 	protected $primaryKey = null;
