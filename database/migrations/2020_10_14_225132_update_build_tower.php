322f8e3c2ebecfcd1415d5b83febf0acc1544d56
derpierre65 <git@derpierre65.de>
2021-05-13 20:03:35 +0200

    updated migration files

--- database/migrations/2020_10_14_225132_update_build_tower.php
+++ database/migrations/2020_10_14_225132_update_build_tower.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Build;
+use App\Models\Build\BuildTower;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,6 +10,49 @@ class UpdateBuildTower extends Migration
 {
     public function up()
     {
+	    $builds = Build::query()->with('waves')->get();
+
+	    /** @var Build $build */
+	    foreach ( $builds as $build ) {
+		    // get build waves
+		    $waves = $build->waves()->orderBy('waveID')->get()->pluck('waveID');
+
+		    // delete old towers
+		    BuildTower::query()
+			    ->where([
+				    'fk_build' => $build->getKey(),
+			    ])
+			    ->where('fk_buildwave', '>', count($waves))
+			    ->delete();
+
+		    // update waves there already exists with the wave id
+		    foreach ( $waves as $key => $waveID ) {
+			    BuildTower::query()
+				    ->where([
+					    'fk_build' => $build->getKey(),
+					    'fk_buildwave' => $key + 1,
+				    ])
+				    ->update([
+					    'fk_buildwave' => $waveID,
+				    ]);
+		    }
+
+		    // create the wave 0 as real wave
+		    $wave = $build->waves()->create([
+			    'name' => 'Build',
+		    ]);
+
+		    // update towers with wave 0
+		    BuildTower::query()
+			    ->where([
+				    'fk_build' => $build->getKey(),
+				    'fk_buildwave' => 0,
+			    ])
+			    ->update([
+				    'fk_buildwave' => $wave->getKey(),
+			    ]);
+	    }
+
 	    Schema::table('build_tower', function (Blueprint $table) {
 		    $table->renameColumn('fk_tower', 'towerID');
 		    $table->renameColumn('fk_buildwave', 'buildWaveID');
